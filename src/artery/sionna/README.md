dabf35dca410a4b59bd7cf42201accdfdc655156
AshFungor <evgenynarkhov2@gmail.com>
2025-11-09 04:05:03 +0000

    sionna: further into scene objects

--- src/artery/sionna/README.md
+++ src/artery/sionna/README.md
@@ -1,4 +1,12 @@
 # Prerequisites (installation)
 
-Provide execution context(either run-artery script of cmake build command)
-with python environment that contains all packages from requirements.
+To enable Sionna integration, you should:
+- create virtual python environment (preferred), or have access to system site-packages installation
+- install `requirements.txt`
+- activate environment (if you use venv) and ensure that build process will run with correct python installation
+
+Then, configure project as usual with `WITH_SIONNA` set to `ON`. This will:
+- try to configure `Mitsuba3` and its dependencies
+- enable additional projects from `extern/`
+- finally, add sionna to project build
+
--- src/artery/sionna/bridge/SceneObject.h
+++ src/artery/sionna/bridge/SceneObject.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <mitsuba/core/fwd.h>
+#include <mitsuba/render/mesh.h>
 #include <pybind11/pybind11.h>
-#include <mitsuba/core/vector.h>
+
+#include "Helpers.h"
+#include "Material.h"
 
 namespace artery {
 
@@ -11,7 +15,73 @@ namespace artery {
         // physically preset in scene. Scene objects are bound to mitsuba meshes,
         // that are used to support INET's scene objects' configuration.
         // defined meshes might be found here: https://github.com/NVlabs/sionna-rt/blob/main/src/sionna/rt/scene.py
-        class SceneObject {};
+        MI_VARIANT class SceneObject : pybind11::object {
+        public:
+            using Point3d = typename mitsuba::CoreAliases<Float>::Point3d;
+            using Point3f = typename mitsuba::CoreAliases<Float>::Point3f;
+            using Vector3f = typename mitsuba::CoreAliases<Float>::Vector3f;
+            using Mesh = typename mitsuba::Mesh<Float, Spectrum>;
+
+            SceneObject(pybind11::object&& obj);
+
+            /**
+             * @brief Create \ref SceneObject from Mitsuba mesh, provided path to valid .obj or .phy file.
+             * 
+             * @param fname Filename with mesh.
+             * @param material Radio material to assign to this object.
+             * @param name Name for this object.
+             */
+            static SceneObject createFromMesh(const std::string& fname, const RadioMaterialBase& material, const std::string& name = "");
+            static SceneObject createFromMesh(const Mesh& mesh, const RadioMaterialBase& material, const std::string& name = "");
+
+            const std::string& name() const;
+            std::uint32_t objectId() const;
+
+            void setPosition(const Point3d& pos);
+            const Point3d& position() const;
+
+            void setOrientation(const Point3f& angles);
+            const Point3f& orientation() const;
+
+            void setScaling(const Vector3f& scale);
+            const Vector3f& scaling() const;
+
+            void lookAt(const Point3f& target);
+
+            void setRadioMaterial(const RadioMaterialBase& mat);
+            const RadioMaterialBase& radioMaterial() const;
+        };
+
+        MI_VARIANT SceneObject<Float, Spectrum>::SceneObject(pybind11::object&& obj)
+            : pybind11::object{std::move(obj)}
+        {}
+
+        MI_VARIANT SceneObject<Float, Spectrum> SceneObject<Float, Spectrum>::createFromMesh(const std::string& fname, const RadioMaterialBase& material, const std::string& name) {
+            using namespace pybind11::literals;
+
+            pybind11::object sceneObject = sionna::sionnaRt().attr("SceneObject");
+            return sceneObject(
+                "fname"_a = fname,
+                "material"_a = material,
+                "name"_a = name
+            );
+        }
+
+        MI_VARIANT const std::string& SceneObject<Float, Spectrum>::name() const {
+            return sionna::access<std::string>(this, "name");
+        }
+
+        MI_VARIANT std::uint32_t SceneObject<Float, Spectrum>::objectId() const {
+            return sionna::access<std::uint32_t>(this, "object_id");
+        }
+
+        MI_VARIANT void SceneObject<Float, Spectrum>::setPosition(const Point3d& pos) {
+            sionna::set(this, "position", pos);
+        }
+
+        MI_VARIANT const typename SceneObject<Float, Spectrum>::Point3d& SceneObject<Float, Spectrum>::position() const {
+
+        }
 
     }  // namespace sionna
 
